a967bd617f35ccd617caf444386aa438114adf4e
wadhen <j.afonso.rato@gmail.com>
2025-06-19 15:03:00 +0100

    redoing stuff

---
+++ verifier-integration/js/index.js
@@ -0,0 +1,113 @@
+const path = require("path");
+const express = require("express");
+const { auth, resolver, protocol } = require("@iden3/js-iden3-auth");
+const getRawBody = require("raw-body");
+const cors = require('cors');
+const app = express();
+const port = 8080;
+
+app.use(express.static("../static"));
+app.use(cors());
+
+app.get("/api/sign-in", (req, res) => {
+  console.log("get Auth Request");
+  getAuthRequest(req, res);
+});
+
+app.post("/api/callback", (req, res) => {
+  console.log("callback");
+  callback(req, res);
+});
+
+app.listen(port, () => {
+  console.log("server running on port 8080");
+});
+
+// Create a map to store the auth requests and their session IDs
+const requestMap = new Map();
+
+// GetQR returns auth request
+async function getAuthRequest(req, res) {
+  // Audience is verifier id
+  const hostUrl = "https://fa65-79-168-104-100.ngrok-free.app";
+  const sessionId = 1;
+  const callbackURL = "/api/callback";
+  const audience =
+    "did:polygonid:polygon:amoy:2qQ68JkRcf3xrHPQPWZei3YeVzHPP58wYNxx2mEouR";
+
+  const uri = `${hostUrl}${callbackURL}?sessionId=${sessionId}`;
+
+  // Generate request for basic authentication
+  const request = auth.createAuthorizationRequest("test flow", audience, uri);
+
+
+  // Add request for a specific proof
+  const proofRequest = {
+    id: 1,
+    circuitId: "credentialAtomicQuerySigV2",
+    query: {
+      allowedIssuers: ["*"],
+      type: "KYCAgeCredential",
+      context:
+        "https://raw.githubusercontent.com/wadhen/PrivadoIdCredentials/refs/heads/main/kyc-v3.json-ld",
+      credentialSubject: {
+        birthday: {
+          $lt: 20000101,
+        },
+      },
+    },
+  };
+  const scope = request.body.scope ?? [];
+  request.body.scope = [...scope, proofRequest];
+
+  // Store auth request in map associated with session ID
+  requestMap.set(`${sessionId}`, request);
+
+  return res.status(200).set("Content-Type", "application/json").send(request);
+}
+
+// Callback verifies the proof after sign-in callbacks
+async function callback(req, res) {
+  // Get session ID from request
+  const sessionId = req.query.sessionId;
+
+  // get JWZ token params from the post request
+  const raw = await getRawBody(req);
+  const tokenStr = raw.toString().trim();
+
+  const keyDIR = "../keys";
+
+  const resolvers = {
+    ["polygon:amoy"]: new resolver.EthStateResolver(
+    "https://polygon-amoy.g.alchemy.com/v2/vM6897Q8uJegP-_09TmUcuklkRu6k-aX",
+    "0x1a4cC30f2aA0377b0c3bc9848766D90cb4404124"
+  ),
+  ["privado:main"]: new resolver.EthStateResolver(
+    "https://rpc-mainnet.privado.id",
+    "0x3C9acB2205Aa72A05F6D77d708b5Cf85FCa3a896"
+  )
+};
+
+  // fetch authRequest from sessionID
+  const authRequest = requestMap.get(`${sessionId}`);
+
+  // EXECUTE VERIFICATION
+  const verifier = await auth.Verifier.newVerifier({
+    stateResolver: resolvers,
+    circuitsDir: path.join(__dirname, keyDIR),
+    ipfsGatewayURL: "https://ipfs.io",
+  });
+
+  try {
+    const opts = {
+      AcceptedStateTransitionDelay: 5 * 60 * 1000, // 5 minute
+    };
+    authResponse = await verifier.fullVerify(tokenStr, authRequest, opts);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+  return res
+    .status(200)
+    .set("Content-Type", "application/json")
+    .send(authResponse);
+}
